243ee4d325786efca64ca9782239da87c803c26e
suhellynaraujo <github.interface@gmail.com>
2024-04-16 21:37:40 -0300

    if

---
+++ primerioPrograma/src/condicional.java
@@ -0,0 +1,13 @@
+public class condicional {
+    public static void main(String[] args) {
+        int anoDeLancamento = 2024;
+        boolean incluidoNoPlano = true;
+        double notaDoFilme = 8.1;
+
+        if (anoDeLancamento >= 2024){
+            System.out.println("Lançamento que os clientes estão curtindo:");
+        }
+
+
+    }
+}
