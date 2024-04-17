d2ac2e3796f44f1256c3699afef04578ad30fe0d
suhellynaraujo <github.interface@gmail.com>
2024-04-16 22:10:54 -0300

    Locale.setDefault(Locale.US);

--- primerioPrograma/src/Leitura.java
+++ primerioPrograma/src/Leitura.java
@@ -1,7 +1,11 @@
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Leitura {
     public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+
         Scanner leitura = new Scanner(System.in);
 
         System.out.println("Digite seu filme favorito: ");
