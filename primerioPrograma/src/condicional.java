11a22859fa1d7f5b274bc8b6e52069ab800e50e2
suhellynaraujo <github.interface@gmail.com>
2024-04-16 21:44:57 -0300

    if/else com || OU

--- primerioPrograma/src/condicional.java
+++ primerioPrograma/src/condicional.java
@@ -1,13 +1,20 @@
 public class condicional {
     public static void main(String[] args) {
         int anoDeLancamento = 2024;
-        boolean incluidoNoPlano = true;
+        boolean incluidoNoPlano = false;
         double notaDoFilme = 8.1;
+        String tipoPlano = "normal";
 
         if (anoDeLancamento >= 2024){
             System.out.println("Lançamento que os clientes estão curtindo:");
         }else {
-            System.out.println("Filme retrô que vale apena assistir:");
+            System.out.println("Filme retrô que vale apena assistir!");
+        }
+
+        if (incluidoNoPlano == true || tipoPlano.equals("plus")){
+            System.out.println("Filme Liberado!");
+        }else {
+            System.out.println("Deve pagar a locação!");
         }
 
 
