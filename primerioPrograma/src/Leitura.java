ab12e4e5d12dd2af3af4a3f428868b006ed496b7
suhellynaraujo <github.interface@gmail.com>
2024-04-16 22:05:26 -0300

    leitura de dados double com Scanner

--- primerioPrograma/src/Leitura.java
+++ primerioPrograma/src/Leitura.java
@@ -4,13 +4,18 @@ public class Leitura {
     public static void main(String[] args) {
         Scanner leitura = new Scanner(System.in);
 
-        System.out.println("Digite seu filme favorito:");
+        System.out.println("Digite seu filme favorito: ");
         //leitura da entrada de dados digitado pelo usuario
         String filme = leitura.nextLine();
-        System.out.println(filme);
 
-        System.out.println("Qual o ano de lançamento?");
+        System.out.println("Qual o ano de lançamento? ");
         int anoDeLancamento = leitura.nextInt();
-        
+
+        System.out.println("Diga sua avaliação para o filme ");
+        double avaliacao =leitura.nextDouble();
+
+        System.out.println(filme);
+        System.out.println(anoDeLancamento);
+        System.out.println(avaliacao);
     }
 }
