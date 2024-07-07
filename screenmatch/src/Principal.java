e9366893617d21e6e74b56b3e5223f1cef502a33
suhellynaraujo <github.interface@gmail.com>
2024-07-07 19:26:24 -0300

    herança e sobrescrita de métodos

--- screenmatch/src/Principal.java
+++ screenmatch/src/Principal.java
@@ -1,4 +1,5 @@
 import br.com.alura.screenmatch.modelos.Filme;
+import br.com.alura.screenmatch.modelos.Serie;
 
 public class Principal {
     public static void main(String[] args) {
@@ -9,6 +10,7 @@ public class Principal {
         meuFilme.setNome("O poderoso chefão");
         meuFilme.setAnoLancamento(1970);
         meuFilme.setDuracaoEmMinutos(188);
+        System.out.println("Duração do Filme: " + meuFilme.getDuracaoEmMinutos());
 
         meuFilme.exibeFichaTecnica();
         meuFilme.avalia(8);
@@ -19,5 +21,15 @@ public class Principal {
        // meuFilme.somaAvaliacoes = 10;
        // meuFilme.totalDeAvaliacao = 1;
        // System.out.println(meuFilme.pegaMedia());
+
+        Serie lost = new Serie();
+        lost.setNome("Lost");
+        lost.setAnoLancamento(2000);
+        lost.exibeFichaTecnica();
+        lost.setTemporadas(10);
+        lost.setEpisodioPorTemporada(10);
+        lost.setMinutoPorEpsodio(50);
+        System.out.println("Duração par amaratonar a série: " + lost.getDuracaoEmMinutos());
     }
+
 }
---
+++ screenmatch/src/br/com/alura/screenmatch/modelos/Serie.java
@@ -0,0 +1,50 @@
+package br.com.alura.screenmatch.modelos;
+//toda série é um titulo
+public class Serie extends Titulo {
+    private int temporadas;
+    private boolean ativa;
+    private int episodioPorTemporada;
+    private int minutoPorEpsodio;
+
+    public int getTemporadas() {
+        return temporadas;
+    }
+
+    public void setTemporadas(int temporadas) {
+        this.temporadas = temporadas;
+    }
+
+    public boolean isAtiva() {
+        return ativa;
+    }
+
+    public void setAtiva(boolean ativa) {
+        this.ativa = ativa;
+    }
+
+    public int getEpisodioPorTemporada() {
+        return episodioPorTemporada;
+    }
+
+    public void setEpisodioPorTemporada(int episodioPorTemporada) {
+        this.episodioPorTemporada = episodioPorTemporada;
+    }
+
+    public int getMinutoPorEpsodio() {
+        return minutoPorEpsodio;
+    }
+
+    public void setMinutoPorEpsodio(int minutoPorEpsodio) {
+        this.minutoPorEpsodio = minutoPorEpsodio;
+    }
+ // subrescrita de método - herda da superclasse , podendo ou não alterar o método
+    @Override
+    public int getDuracaoEmMinutos() {
+        return temporadas * episodioPorTemporada * minutoPorEpsodio;
+    }
+
+    // superclasse (classe mãe) - generalização
+    // subclasse (classes filhas) - especialização
+    // Atributos (campos)
+    // Metodos (fuções)
+}
