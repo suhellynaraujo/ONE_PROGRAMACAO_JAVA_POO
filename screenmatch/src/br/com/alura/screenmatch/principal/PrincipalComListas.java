17d07791e38dd4fe17078417b4ae2ea7f55174a6
suhellynaraujo <github.interface@gmail.com>
2024-07-09 21:08:31 -0300

    List

--- screenmatch/src/br/com/alura/screenmatch/principal/PrincipalComListas.java
+++ screenmatch/src/br/com/alura/screenmatch/principal/PrincipalComListas.java
@@ -4,9 +4,7 @@ import br.com.alura.screenmatch.modelos.Filme;
 import br.com.alura.screenmatch.modelos.Serie;
 import br.com.alura.screenmatch.modelos.Titulo;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
+import java.util.*;
 
 public class PrincipalComListas {
     public static void main(String[] args) {
@@ -18,7 +16,7 @@ public class PrincipalComListas {
         maisUmFilme.avalia(10);
         Serie lost = new Serie("Lost", 200);
 
-        ArrayList<Titulo> lista = new ArrayList<>();
+        List<Titulo> lista = new LinkedList<>();
         lista.add(meuFilme);
         lista.add(outroFilme);
         lista.add(maisUmFilme);
