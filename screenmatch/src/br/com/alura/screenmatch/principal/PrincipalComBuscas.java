be4619955e4e1f64086c9ca1b7d1a93db0913e5f
suhellynaraujo <github.interface@gmail.com>
2024-07-14 14:59:47 -0300

    Request - requisição para a api

---
+++ screenmatch/src/br/com/alura/screenmatch/principal/PrincipalComBuscas.java
@@ -0,0 +1,29 @@
+package br.com.alura.screenmatch.principal;
+
+import br.com.alura.screenmatch.modelos.Titulo;
+import com.google.gson.Gson;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Scanner;
+
+public class PrincipalComBuscas {
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+        Scanner leitura = new Scanner(System.in);
+        System.out.println("Digite um nome de um filme para a busca: ");
+        var busca = leitura.nextLine();
+
+        String endereco = "https://www.omdbapi.com/?t="+ busca + "&apikey=513daa7b";
+        HttpClient client = HttpClient.newHttpClient();
+        // requisição para a api
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(endereco))
+                .build();
+
+    }
+}
+
